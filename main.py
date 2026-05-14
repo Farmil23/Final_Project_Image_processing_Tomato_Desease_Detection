b83e351eb00ddff744c64ac5c5ee95ea44d0bbd6
Farhan kamil Hermansyah <affschannel@gmail.com>
2026-05-14 21:03:40 +0700

    STRUCTURE PROJECT

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    # 1. Load Data (Panggil fungsi Darent)
+    # 2. Preprocessing (Panggil fungsi Ratu/Najwa/Disa/Farhan)
+    # 3. Extraction (Panggil fungsi Fase 3)
+    # 4. Classification (Fase 4)
+    print("Pipeline Deteksi Penyakit Tomat Siap!")
+
+if __name__ == "__main__":
+    main()
